08cb09f8ce62840e5dc0462ca56e774deade03fd
bbbilibili <1971446420@qq.com>
2020-10-13 23:43:01 +0800

    sort()  张小飞

---
+++ src/main/sort.java
@@ -0,0 +1,19 @@
+package main;
+
+public class sort {
+    public void Sort(int[] arr) {
+   	 for(int j = 0;j < arr.length;j++) {
+   		 for(int i = 0;i < arr.length-1;i++)
+   		 if(arr[i]<arr[i+1]) {
+   			 int temp = arr[i+1];
+   			 arr[i+1] = arr[i];
+   			 arr[i] =temp;
+   		 }
+   	 }
+   	 for(int j = 0;j < arr.length;j++) {
+   		 System.out.print(arr[j]+" ");
+    }
+   }
+    
+    
+}
