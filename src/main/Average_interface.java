c18c6b034ff3a1d549737af54d9baba7974b8914
bainingxuan <150926270262@163.com>
2020-10-30 10:28:01 +0800

    修改

--- src/main/Average_interface.java
+++ src/main/Average_interface.java
@@ -1,5 +1,11 @@
 package main;
 
+/*
+ * 于虎
+ * 
+ * 平均数
+ * 
+ * */
 public interface Average_interface {
 	public int [] aver(int [] array);
 }
--- src/main/Max_interface.java
+++ src/main/Max_interface.java
@@ -1,5 +1,9 @@
 package main;
-
+/*
+ * 姜晓林
+ * 
+ * 最大值
+ * */
 public interface Max_interface {
 	public abstract int [] print(int [] array);
 }
